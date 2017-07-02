1b43160a005e12e0086158b94d3ef2e1570da0cd
OrKoN <alexei.rudenko@gmail.com>
2017-07-02 12:14:12 +0200

    Refactor test data

--- test/Graph.spec.js
+++ test/Graph.spec.js
@@ -1,68 +1,23 @@
 const Graph = require('../src/Graph');
 const assert = require('assert');
+const _ = require('lodash');
 
 describe('Graph', () => {
   it('should build a graph', () => {
-    const data0 = {
-      id: '00',
-      location: [9, 48],
-      firstEdgeId: -1,
-    };
-    const data1 = {
-      id: '01',
-      location: [9, 48],
-      firstEdgeId: -1,
-    };
-    const data2 = {
-      id: '02',
-      location: [9, 48],
-      firstEdgeId: -1,
-    };
-    const data3 = {
-      id: '03',
-      location: [9, 48],
-      firstEdgeId: -1,
-    };
-    const data4 = {
-      id: '04',
-      location: [9, 48],
-      firstEdgeId: -1,
-    };
-    const data5 = {
-      id: '05',
-      location: [9, 48],
-      firstEdgeId: -1,
-    };
-    const data6 = {
-      id: '06',
-      location: [9, 48],
-      firstEdgeId: -1,
-    };
-    const data7 = {
-      id: '07',
-      location: [9, 48],
-      firstEdgeId: -1,
-    };
-
+    const data = _.range(8).map(i => {
+      return {
+        id: `node${i}`,
+        location: [9, 48],
+        firstEdgeId: -1,
+      }
+    })
     const g = new Graph();
-    g.addVertex('node0');
-    g.addVertex('node1');
-    g.addVertex('node2');
-    g.addVertex('node3');
-    g.addVertex('node4');
-    g.addVertex('node5');
-    g.addVertex('node6');
-    g.addVertex('node7');
-    g.setVertexValue('node0', data0);
-    g.setVertexValue('node1', data1);
-    g.setVertexValue('node2', data2);
-    g.setVertexValue('node3', data3);
-    g.setVertexValue('node4', data4);
-    g.setVertexValue('node5', data5);
-    g.setVertexValue('node6', data6);
-    g.setVertexValue('node7', data7);
+    data.forEach(node => {
+      g.addVertex(node.id);
+      g.setVertexValue(node.id, node);
+    });
 
-    assert.deepEqual(g.getVertexValue('node1'), data1);
+    assert.deepEqual(g.getVertexValue('node1'), data[1]);
 
     g.addEdge('node0', 'node3');
     g.addEdge('node1', 'node3');
@@ -91,7 +46,7 @@ describe('Graph', () => {
       edge: g.nodes.get(nodeId).firstEdgeId
     }, {
       i: nodeId,
-      id: data0.id,
+      id: data[0].id,
       lng: 9,
       lat: 48,
       edge: 0,
@@ -113,6 +68,6 @@ describe('Graph', () => {
     assert.equal(g.adjacent('node1', 'node2'), true);
     assert.equal(g.adjacent('node3', 'node6'), false);
 
-    assert.deepEqual(g.neighbors('node0'), ['03', '07']);
+    assert.deepEqual(g.neighbors('node0'), ['node3', 'node7']);
   });
 });
